57c36ff93dab06add4b898de6590030f42c996ac
unknown <lucacasonato@yahoo.com>
2018-06-02 18:20:20 +0200

    More button types

--- mdl/button.go
+++ mdl/button.go
@@ -23,9 +23,43 @@ func NewButtonDefault(caption string, buttontype ...string) *gowd.Element {
 	return btn
 }
 
-//NewButtonDefault creates a new standard mdl <button> element
+//NewButtonFab creates a new fab mdl <button> element
 func NewButtonFab(icon string, enabled bool, buttontype ...string) *gowd.Element {
 	btn := NewElement("button", "mdl-button mdl-js-button mdl-button--fab"+strings.Join(buttontype, ", "))
+
+	if icon != "" {
+		btnIcon := NewElement("i", "material-icons")
+		btnIcon.SetText(icon)
+		btn.AddElement(btnIcon)
+	} else {
+		return nil
+	}
+	if !enabled {
+		btn.Disable()
+	}
+	return btn
+}
+
+//NewButtonFabMini creates a new mini fab mdl <button> element
+func NewButtonFabMini(icon string, enabled bool, buttontype ...string) *gowd.Element {
+	btn := NewElement("button", "mdl-button mdl-js-button mdl-button--fab button--mini-fab"+strings.Join(buttontype, ", "))
+
+	if icon != "" {
+		btnIcon := NewElement("i", "material-icons")
+		btnIcon.SetText(icon)
+		btn.AddElement(btnIcon)
+	} else {
+		return nil
+	}
+	if !enabled {
+		btn.Disable()
+	}
+	return btn
+}
+
+//NewButtonIcon creates a new icon mdl <button> element
+func NewButtonIcon(icon string, enabled bool, buttontype ...string) *gowd.Element {
+	btn := NewElement("button", "mdl-button mdl-js-button mdl-button--icon"+strings.Join(buttontype, ", "))
 
 	if icon != "" {
 		btnIcon := NewElement("i", "material-icons")
