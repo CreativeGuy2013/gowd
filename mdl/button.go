820f2dde5e55212e4da3e7c2f2bfaa6b80f963e3
jaap aarts <jaap.aarts1@gmail.com>
2018-06-02 17:54:58 +0200

    \jun-02-2018/ made function NewFab

--- mdl/button.go
+++ mdl/button.go
@@ -10,7 +10,7 @@ const (
 	//ButtonPrimary primary bootstrap button
 	ButtonPrimary = "btn-primary"
 
-	ButtonColoures = "mdl-button--colored"
+	ButtonColoured = "mdl-button--colored"
 	//ButtonColouredFab = "mdl-button mdl-js-button mdl-button--fab mdl-button--colored"
 	//ButtonColouredFab = "mdl-button mdl-js-button mdl-button--fab mdl-button--colored"
 	//ButtonColouredFab = "mdl-button mdl-js-button mdl-button--fab mdl-button--colored"
