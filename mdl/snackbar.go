7d4d40e01a0643f6279af4fb509cef970ca1767b
jaap aarts <jaap.aarts1@gmail.com>
2018-06-03 18:08:47 +0200

    \jun-03-2018/ make a snackbar.open

--- mdl/snackbar.go
+++ mdl/snackbar.go
@@ -10,14 +10,12 @@ type Snackbar struct {
 	*gowd.Element
 	textElement   *gowd.Element
 	buttonElement *gowd.Element
-	eventHandler  *gowd.Element
 	message       string
 	timeout       int
-	actionhandler string
 	actionText    string
 }
 
-func NewSnackbar(message string, timeout int, eventlistner gowd.EventHandler) *Snackbar {
+func NewSnackbar(message string, timeout int, actionText string, eventlistner gowd.EventHandler) *Snackbar {
 	snackbar := new(Snackbar)
 
 	snackbar.Element = NewElement("div", "mdl-js-snackbar mdl-snackbar")
@@ -27,13 +25,17 @@ func NewSnackbar(message string, timeout int, eventlistner gowd.EventHandler) *S
 	snackbar.buttonElement = NewElement("button", "mdl-snackbar__action")
 	snackbar.buttonElement.OnEvent(gowd.OnClick, eventlistner)
 
-	snackbar.actionhandler = fmt.Sprintf("fire_event('onclick',document.getElementById(%s));", snackbar.buttonElement.GetID())
-
-	snackbar.eventHandler = NewElement("script", "")
-	ExecJS(fmt.Sprintf("var handler%s = function(event) {%s};", snackbar.Element.GetID(), snackbar.actionhandler))
 	snackbar.Element.AddElement(snackbar.textElement)
 	snackbar.Element.AddElement(snackbar.buttonElement)
-	snackbar.Element.AddElement(snackbar.eventHandler)
+
+	snackbar.message = message
+	snackbar.timeout = timeout
+	snackbar.actionText = actionText
 
 	return snackbar
+}
+
+func (snackbar *Snackbar) Open() {
+	ExecJS(fmt.Sprintf("document.querySelector('#_div4').MaterialSnackbar.showSnackbar({message: '%s',timeout: %d,actionHandler: function(event) {},actionText: '%s'});", snackbar.message, snackbar.timeout, snackbar.actionText))
+
 }
