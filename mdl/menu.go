10889a157083e8b993c5fff35493bd7059d60eb4
unknown <lucacasonato@yahoo.com>
2018-06-05 19:34:46 +0200

    Changed some stuff to do with menus

--- mdl/menu.go
+++ mdl/menu.go
@@ -7,16 +7,19 @@ import (
 )
 
 //NewMenu creates a new menu
-func NewMenu(bindElement *gowd.Element, options []*gowd.Element, menuModifiers ...string) *gowd.Element {
+func NewMenu(bindElement *gowd.Element, options []*gowd.Element, menuModifiers ...string) (*gowd.Element, []*gowd.Element) {
 	newMenu := new(gowd.Element)
 	newMenu = NewElement("ul", "mdl-menu mdl-js-menu "+strings.Join(menuModifiers, " "))
 	newMenu.SetAttribute("data-mdl-for", bindElement.GetID())
+
+	var outElements []*gowd.Element
 
 	for _, element := range options {
 		newOption := NewElement("li", "mdl-menu__item")
 		newOption.AddElement(element)
 		newMenu.AddElement(newOption)
+		outElements = append(outElements, newOption)
 	}
 
-	return newMenu
+	return newMenu, outElements
 }
